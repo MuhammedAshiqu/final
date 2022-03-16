17e67f6f8bad4518600874b4cee2e498595b3669
MuhammedAshiqu <ashiqutk321@gmail.com>
2022-03-16 12:49:07 +0530

    Clean Code

---
+++ frontend/src/Components/USERS/Tabs/SideTab.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+import {Tabs,Tab} from 'react-bootstrap'
+import Myorders from '../Myorders/Myorders';
+import './sidetab.css'
+const Sidetab = ({nwUser}) => {
+  const [key, setKey] = useState('home');
+  return (
+    <Tabs  defaultActiveKey="profile" id="uncontrolled-tab-example" className="mb-3 d-flex justify-content-center">
+    <Tab style={{width:'100%'}} className='bg-light' eventKey="profile" title="Profile">
+    <div style={{color:'#000'}} > 
+      <span> <h1>Name:<i>{nwUser&& nwUser.Name}</i></h1></span>
+      <span> <h3>Email:<i>{nwUser&& nwUser.Email}</i></h3></span>
+      <span> <h1>Phone Number:<i>{nwUser&& nwUser.Phone}</i></h1></span>
+      <span> <h1>Address:<i>{nwUser&& nwUser.Address}</i></h1></span>
+        
+      </div>
+    </Tab>
+  
+    <Tab style={{width:'100%'}} className='bg-light' eventKey="order" title="My Orders" >
+      <div>
+      <Myorders/>
+      </div>
+    </Tab>
+  </Tabs>
+  )
+}
+
+export default Sidetab;
+
